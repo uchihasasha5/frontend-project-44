2d10b5299674040f74bd7409e3f27c517ab090eb
Александр Яковлев <uchihasasha5@mail.ru>
2023-02-26 13:46:59 +0300

    step 6 clear

--- src/brain-calc.js
+++ src/brain-calc.js
@@ -1,10 +1,17 @@
 import readlineSync from 'readline-sync';
 
 export const calcGameData = () => {
+  const massoperations = ['.', '-', '+', '*'];
   const num1 = Math.ceil(Math.random() * 100);
   const num2 = Math.ceil(Math.random() * 100);
-  const regularExpressions = `${num1} + ${num2}`;
-  const correctAnswer = num1 + num2;
+  const randomizeoperetions = Math.ceil(Math.random(massoperations) * 3);
+  // данная константа выбирает один из трех возможных операторов //
+  const operations = massoperations[randomizeoperetions];
+  const regularExpressions = `${num1} ${operations} ${num2}`;
+  let correctAnswer;
+  if (operations === '-') { correctAnswer = num1 - num2; }
+  if (operations === '+') { correctAnswer = num1 + num2; }
+  if (operations === '*') { correctAnswer = num1 * num2; }
   console.log(`Question:${regularExpressions}?`);
   const playerAnswer = readlineSync.question('Your answer:');
   const playerNumber = Number(playerAnswer);
