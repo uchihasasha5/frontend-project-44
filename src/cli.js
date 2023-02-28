b0fdd220da48b3b708dee947746abf4e9bcfbd72
Александр Яковлев <uchihasasha5@mail.ru>
2023-03-01 00:59:18 +0300

    x

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export { welcome };
