efe3c38152bc7d73cb1229a529dcdecb09ab1368
Александр Яковлев <uchihasasha5@mail.ru>
2023-03-01 21:23:11 +0300

    bug fix gcd game

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -21,10 +21,11 @@ const getGcdGameData = () => {
     }
     return c;
   };
-  const result = getGCD(randomNumber1, randomNumber2);
+  let result = getGCD(randomNumber1, randomNumber2);
   if (result === playerNumber) {
     return 1;
   }
+  if (randomNumber1 === randomNumber2) { result = Number(randomNumber1); }
   console.log(`'${playerNumber}' is wrong answer ;(. Correct answer '${result}'`);
   return 'wrong!';
 };
