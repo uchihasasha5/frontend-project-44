5134b3bf3e8e377678e2f7f6ce9bba600a26445c
Александр Яковлев <uchihasasha5@mail.ru>
2023-03-22 22:15:49 +0300

    x

--- src/utilits.js
+++ src/utilits.js
@@ -1,1 +1,3 @@
-export const randomNum = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+const randomNum = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+
+export default randomNum;
